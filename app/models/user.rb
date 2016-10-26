48699fc24d3a82b0631f0aaea7004424cad1b24d
dzrcarl <nuclearcarl@hotmail.com>
2016-10-26 01:29:18 -0700

    validation and test case for user initial setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+
+  # names should not be empty
+  validates :fname, :lname, :nickname, presence: true, length: {maximum:50}
+  validates :nickname, :uniqueness => {:message => "already taken"}
+
+  # email should not be empty
+  validates :email, presence: true, lenght: {maximum: 255}, :uniqueness => {message => "has been registered"}
+
+
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,55 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new(fname: "Example", lname: "User", nickname: "Demo", email: "user@example.com")
+  end
+
+  # test for initial setup
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  #test for the user name
+  test "fname should be presnet" do
+    @user.fname = "  "
+    assert_not @user.valid?
+  end
+  test "lname should be presnet" do
+    @user.lname = "  "
+    assert_not @user.valid?
+  end
+  test "nickname should be presnet" do
+    @user.nickname = "  "
+    assert_not @user.valid?
+  end
+
+  #presence of email
+  test "email should be presnet" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  #length test
+  test "fname should not be too long" do
+    @user.fname = "a"*51
+    assert_not @user.valid?
+  end
+  test "lname should not be too long" do
+    @user.lname = "a"*51
+    assert_not @user.valid?
+  end
+  test "nickname should not be too long" do
+    @user.nickname = "a"*51
+    assert_not @user.valid?
+  end
+  test "email should not be too long" do
+    @user.email = "a"*244 + "@example.com"
+    assert_not @user.valid?
+
+  end
+
   # test "the truth" do
   #   assert true
   # end
