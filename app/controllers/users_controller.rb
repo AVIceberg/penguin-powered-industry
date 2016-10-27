6f09d77137fdf931a9fa7b74663d185252e5e25e
dzrcarl <nuclearcarl@hotmail.com>
2016-10-27 01:53:08 -0700

    sign up page is live!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      redirect_to user_path(params[:id]), :notice => "Success!"
+    else
+      render "new"
+    end
   end
 
 
