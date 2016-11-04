2789b9865d2b9850894c7106b30b74ad911d3351
Alistair Vanbergen <AlistairVanbegen@Gmail.com>
2016-11-04 15:20:35 -0700

    Added a few unit tests for users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(fname: "Example", lname: "User", nickname: "Demo", email: "user@example.com", password: "tested", password_confirmation: "tested")
+    @user = User.new(fname: "Example", lname: "User", nickname: "Demo", email: "user@example.com", password: "tested", password_confirmation: "tested", level: 1, max: 0)
   end
 
   # test for initial setup
@@ -25,13 +25,28 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  # Level test
+  test "level > 1" do
+    @user.level = 0
+    assert_not @user.valid?
+  end
+
+  # Max Tests
+  test "max >= 0" do
+    assert @user.valid?
+    @user.max = -1
+    assert_not @user.valid?
+    @user.max = 20000000
+    assert_not @user.valid?
+  end
+
   #presence of email
   test "email should be present" do
     @user.email = ""
     assert_not @user.valid?
   end
 
-  #length test (shoudl be failed)
+  #length test (should fail)
   test "fname should not be too long" do
     @user.fname = "a"*51
     assert_not @user.valid?
@@ -49,7 +64,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  #validate email formate (shoudl be valid)
+  #validate email format (should be valid)
   test "email validation should accept valid address" do
     valid_address = %w[user@example.com USER@sfu.ca U_SE-RRR@sfu.org.ca first.last@qq.im alex+tom@sample.cn]
     valid_address.each do |valid_address|
