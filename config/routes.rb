80c545271c6701f3907181685f1eb052471a2814
Godwin Stephens <godwin.skye@gmail.com>
2016-11-06 16:50:14 -0800

    About page up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
-  post '/signup', to: 'users#new'
+ # post '/signup', to: 'users#new'
   get '/login', to: 'welcome#login'
+  get '/about', to: 'welcome#about'
 
   resources :application do
     member do
