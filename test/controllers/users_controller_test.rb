48779edc20306e8800504de60443dc3542081826
gza21 <gza21@sfu.ca>
2016-11-07 22:13:44 -0800

    test2

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,48 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+
+class UsersControllerTest < ActionController::TestCase
+
   test "should get new" do
-    get signup_path
+    get :new
     assert_response :success
   end
+
+  test "enter user list" do
+
+    #@user.save
+    session[:id]=1
+    get :index
+    assert_response :success
+
+  end
+
+  test "enter edit page" do
+
+    #@user.save
+    session[:id]=0
+    get :edit , id: session[:id]
+    assert_response :success
+
+  end
+
+  test "edit page is accessible if logged in" do
+    session[:id] = 1
+    get :edit, id: session[:id]
+    assert_template 'users/edit'
+  end
+
+  test "edit page for other users is inaccessible" do
+    session[:id] = 1
+    get :edit, id: 2
+    assert_template 'users/show'
+  end
+
+end
+
+class UsersControllerTest1 < ActionDispatch::IntegrationTest
+
+
 
   test "valid signup and redirect" do
   	get signup_path
@@ -16,20 +54,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   	assert_template 'users/show'
   end
 
-  test "edit page is accessible if logged in" do
-    session[:id] = 1
-    get edit_application, id: session[:id]
-    assert_template 'application/edit'
-  end
 
-  test "edit page for other users is inaccessible" do
-    session[:id] = 1
-    get edit_application, id: 2
-    assert_template 'users/show'
-  end
 
   test "admin user creation works" do
-    
+
   end
 
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,32 +1,46 @@
 require 'test_helper'
 
-class WelcomeControllerTest < ActionDispatch::IntegrationTest
+class WelcomeControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
 
+#<<<<<<< HEAD
+
+
+
+
+end
+class WelcomeControllerTest2 < ActionDispatch::IntegrationTest
+
+  setup do
+
+    @user1 = User.new(fname: "Example", lname: "User", nickname: "Demo", email: "user@example.com", password: "tested", password_confirmation: "tested", level: 1, max: 0)
+    @user1.save
+
+  end
+
+
+#=======
   #################################################
   ##Login tests
 
   ##user:nickname and password: something is created beforehand!
 
   test "login invalid user" do
-    post :login, user: { nickname: "doesntexist",
-                      password: "testers" }
+    post login_path, nickname: "doesntexist",   password: "testers"
     follow_redirect!
     assert_template 'welcome/index'
   end
 
   test "login invalid password" do
-    post :login, user: { nickname: "something",
-                      password: "somethings" }
+    post login_path, nickname: "something",    password: "somethings"
     follow_redirect!
     assert_template 'welcome/index'
   end
 
   test "login works" do
-    post :login, user: { nickname: "something",
-                      password: "something" }
+    post login_path, nickname: "Demo",    password: "tested"
     follow_redirect!
     assert_template 'users/show'
   end
@@ -34,14 +48,10 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
 #####################################################
   ##Logout testers
 
-  test "logout works" do
-    session[:id] = 0
-    delete :logout_application
-    follow_redirect!
-    assert_template 'welcome/index'       #assert(if session is defined, login page is NOT accessible);
-  end
+
 
 #####################################################
   ##User List tests and admin tests to be implemented
-  
+
+#>>>>>>> ecd2744c96b0d8fedfdec685a73d81a774107961
 end
