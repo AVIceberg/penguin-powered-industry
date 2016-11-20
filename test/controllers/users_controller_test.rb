67a0619e43083768626aabecbcbd391f6707af38
Alistair Vanbergen <AlistairVanbegen@Gmail.com>
2016-11-19 18:40:28 -0800

    Refactored and added tests from iteration #1 that were broken, incomplete, or missing.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,42 +9,49 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "enter user list" do
-
     #@user.save
-    session[:id]=1
+    session[:id]= users(:Alex).id
     get :index
     assert_response :success
 
   end
 
-  test "enter edit page" do
-
-    #@user.save
-    session[:id]=0
+  test "Valid Edit Page Request" do
+    session[:id]= users(:Kate).id
     get :edit , id: session[:id]
     assert_response :success
-
-  end
-
-  test "edit page is accessible if logged in" do
-    session[:id] = 1
-    get :edit, id: session[:id]
     assert_template 'users/edit'
   end
 
-  test "edit page for other users is inaccessible" do
-    session[:id] = 1
-    get :edit, id: 2
-    assert_template 'users/show'
+  test "Admin can access other edit pages" do
+    session[:id] = users(:Kate).id
+    get :edit, id: 1
+    assert_template 'users/edit'
+  end
+
+  test "Users cannot access other peoples' edit pages" do
+    session[:id] = users(:Alex).id
+    get :edit, id: 0
+    assert_response :redirect
+  end
+
+  test "Admins can access other peoples' edit pages" do
+    session[:id] = users(:Kate).id
+    get :edit, id: 1
+    assert_response :success
+  end
+
+  test "Check user list loads" do
+    get :index, id: users(:Kate)
+    assert_response :success
+    assert_select 'tr', User.all.count + 1
   end
 
 end
 
 class UsersControllerTest1 < ActionDispatch::IntegrationTest
 
-
-
-  test "valid signup and redirect" do
+  test "Valid signup and Redirect" do
   	get signup_path
   	assert_difference 'User.count', 1 do
   		post users_path,  user: { fname: "tester22", lname: "tester22", nickname: "tester22", email: "tester@testers.com",
@@ -54,10 +61,62 @@ class UsersControllerTest1 < ActionDispatch::IntegrationTest
   	assert_template 'users/show'
   end
 
+  test "User logout" do
+    get root_path
+    post login_path, :nickname => "Kate", :password => "testing"
+    follow_redirect!
+    assert_template 'users/show'
+    assert session[:id] == users(:Kate).id
+    delete logout_application_path
+    assert_not session[:id]
+  end
 
+  test "Invalid Profile Edit" do
+    get root_path
+    post login_path, :nickname => "Alex", :password => "testing"
+    follow_redirect!
+    assert_template 'users/show'
+    get edit_user_path, id: session[:id]
+    assert_response :success
+    put user_path, user: { fname: "Alex", lname: "Zheng", nickname: "Alex", email: "a"}
+    assert_template 'users/edit'
+    assert User.find_by_id(session[:id]).email != "a"
+  end
 
-  test "admin user creation works" do
+  test "Valid Profile Edit" do
+    get root_path
+    post login_path, :nickname => "Alex", :password => "testing"
+    follow_redirect!
+    assert_template 'users/show'
+    get edit_user_path, id: session[:id]
+    assert_response :success
+    put user_path, user: { fname: "Alex", lname: "Zheng", nickname: "Alex", email: "a@a.com"}
+    assert_response :redirect
+    assert User.find_by_id(session[:id]).email == "a@a.com"
+  end
 
+  test "Invalid Admin Profile Edit" do
+    get root_path
+    post login_path, :nickname => "Kate", :password => "testing"
+    follow_redirect!
+    assert_template 'users/show'
+    get edit_user_path, id: users(:Alex).id #Not Kate's ID
+    assert_response :success
+    put user_path, user: { fname: "Alex", lname: "Zheng", nickname: "Alex", email: "a", id: users(:Alex).id}
+    assert_template 'users/edit'
+    assert User.find_by_id(users(:Alex).id).email != "a"
+  end
+
+  test "Valid Admin Profile Edit" do
+    get root_path
+    post login_path, :nickname => "Kate", :password => "testing"
+    follow_redirect!
+    assert_template 'users/show'
+    get edit_user_path, id: users(:Alex).id #Not Kate's ID
+    assert_response :success
+    put user_path, user: { fname: "Alex", lname: "Zheng", nickname: "Alex", email: "a@a.com", id: users(:Alex).id }
+    assert_response :redirect
+    #assert User.find_by_id(users(:Alex).id).email == "a@a.com"
   end
 
 end
