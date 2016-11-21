49c136c0c00bf9af8c31804bccf928c9b650d8b0
Alistair Vanbergen <AlistairVanbegen@Gmail.com>
2016-11-21 14:38:12 -0800

    Implemented functional tests for clicking area + save button.

--- test/controllers/game_controller_test.rb
+++ test/controllers/game_controller_test.rb
@@ -4,4 +4,17 @@ class GameControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Clicking Area Generated" do
+    session[:id] = users(:Alex).id
+    get :gamepage
+    assert_select '#clicking-area'
+  end
+
+  test "Save button exists" do
+    session[:id] = users(:Alex).id
+    get :gamepage
+    assert_select '#save-button'
+  end
+
 end
