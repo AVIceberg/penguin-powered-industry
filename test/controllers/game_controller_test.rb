314dd28d419b81b53f6f1337f0eda2f641e3b606
osk666 <osk666@gmail.com>
2016-11-28 23:02:01 +0000

    update

--- test/controllers/game_controller_test.rb
+++ test/controllers/game_controller_test.rb
@@ -27,9 +27,20 @@ class GameControllerTest < ActionController::TestCase
   end
   
   test "newly created building map save" do
+    assert_equal users(:Cat).building_map, 0
     session[:id] = users(:Cat).id
-    get :save, leveledUp: "false", max: users(:Cat).max, toys: users(:Cat).toys, time_left: users(:Cat).timeleft, map: users(:Cat).map.to_json, building_map: users(:Cat).building_map.to_json
+    building = users(:Cat).building_map
+    get :save, leveledUp: "false", max: users(:Cat).max, toys: users(:Cat).toys, time_left: users(:Cat).timeleft, map: users(:Cat).map.to_json, building_map: building.to_json
     assert_response :success
+    count = 0
+    (0..7).each do |i|
+      (0..7).each do |j|
+        if building[i][j] != "0"
+          count = count + 1
+        end
+      end
+    end
+    assert_equal users(:Cat).map, 0
   end
 
 end
